65d1073746701270086fcd73e840486dc8903662
Clàudia Vicente <146286495+claudiavicente@users.noreply.github.com>
2023-12-17 17:45:44 +0100

    Add files via upload

---
+++ fasterq_dump.sh
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\partightenfactor0
+
+\f0\fs26 \cf0 ./fasterq-dump SRR23839035 \
+./fasterq-dump SRR23839036 \
+./fasterq-dump SRR23839037\
+./fasterq-dump SRR23839038 \
+./fasterq-dump SRR23839039 \
+./fasterq-dump SRR23839040 \
+./fasterq-dump SRR23839041 \
+./fasterq-dump SRR23839042 \
+./fasterq-dump SRR23839043 \
+./fasterq-dump SRR23839044 \
+./fasterq-dump SRR23839045 \
+./fasterq-dump SRR23839046 }
---
+++ kallisto.sh
@@ -0,0 +1,35 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;\red220\green161\blue13;}
+{\*\expandedcolortbl;;\cssrgb\c89412\c68627\c3922;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww13980\viewh8400\viewkind0
+\deftab560
+\pard\pardeftab560\slleading20\partightenfactor0
+
+\f0\fs26 \cf0 curl -O {\field{\*\fldinst{HYPERLINK "ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"}}{\fldrslt \cf2 ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz}}\
+kallisto index -i  Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx	Homo_sapiens.GRCh38.cdna.all.fa.gz\
+\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839035 SRR23839035_1.fastq SRR23839035_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839036 SRR23839036_1.fastq SRR23839036_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839037 SRR23839037_1.fastq SRR23839037_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839038 SRR23839038_1.fastq SRR23839038_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839039 SRR23839039_1.fastq SRR23839039_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839040 SRR23839040_1.fastq SRR23839040_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839041 SRR23839041_1.fastq SRR23839041_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839042 SRR23839042_1.fastq SRR23839042_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839043 SRR23839043_1.fastq SRR23839043_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839044 SRR23839044_1.fastq SRR23839044_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839045 SRR23839045_1.fastq SRR23839045_2.fastq\
+\
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -b 20 -t 2 -o kallisto_out/SRR23839046 SRR23839046_1.fastq SRR23839046_2.fastq}
